1bb66ebc1a1bd598d7a821b4a3385cb8e9d979d0
Nikul <Nikul@Anixous-PC>
2016-08-23 15:22:34 +1000

    Add the comments both, header and in-line comments.

--- ITC515_assignment2/src/datamanagement/IStudent.java
+++ ITC515_assignment2/src/datamanagement/IStudent.java
@@ -1,18 +1,22 @@
+/* This interface declared the methods of the student properties such as
+ * student id , first name and last name and unit record and list. 
+ * 
+ */
 package datamanagement;
 
 public interface IStudent 
 {
-    public Integer getID();
+    public Integer getID(); //return the student id
 
-    public String getFirstName();
-    public void setFirstName(String firstName);
+    public String getFirstName(); //Return the student's first name
+    public void setFirstName(String firstName); //Set the student's first name
 
-    public String getLastName();
-    public void setLastName(String lastName);
+    public String getLastName(); //Return the student's last name
+    public void setLastName(String lastName); //set the student's last name
 
-    public void addUnitRecord( IStudentUnitRecord record );
-    public IStudentUnitRecord getUnitRecord( String unitCode );
+    public void addUnitRecord( IStudentUnitRecord record ); //Add the unit record of particular student
+    public IStudentUnitRecord getUnitRecord( String unitCode ); //Get the student's unit record
 
-    public StudentUnitRecordList getUnitRecords();
+    public StudentUnitRecordList getUnitRecords(); //Return the student's unit record list
 
 }
