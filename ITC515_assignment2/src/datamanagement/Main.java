41e9fe65dde2cc1318bc4b05bb8036530fc4bc7a
vedant-raichada <vedantr4@gmail.com>
2016-08-23 15:19:23 +1000

    Changes in the Layout of the code

---
+++ ITC515_assignment2/src/datamanagement/Main.java
@@ -0,0 +1,10 @@
+package datamanagement;
+
+public class Main 
+{
+    public static void main(String[] p) 
+    {
+    	new cgCTL().execute();
+	}    
+}
+//Did the indentation for the lines of code
