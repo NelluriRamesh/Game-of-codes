69c20fb87c3aa6e047ceffef7558dd0be49f3015
BarisPro <bmovoisin@gmail.com>
2016-08-24 23:21:14 +1000

    Delete IUnitLister.java

--- Gameofcodes_v1/ITC515_assignment2/src/datamanagement/IUnitLister.java
+++
@@ -1,8 +0,0 @@
-package datamanagement;
-
-public interface IUnitLister {
-
-    public void clearUnits(); // interface function that can be implemented to clear the units
-    public void addUnit(IUnit unit); // interface function that can be implemented to add a new Unit
-
-}
