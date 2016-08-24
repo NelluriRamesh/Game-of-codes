2af2e4c907a4b89208b1ac081bef5897f638942d
Nikul <Nikul@192.168.1.8>
2016-08-24 17:02:09 +1000

    Added header comments and in-line comments

--- ITC515_assignment2/src/datamanagement/IStudentLister.java
+++ ITC515_assignment2/src/datamanagement/IStudentLister.java
@@ -1,11 +1,16 @@
-package datamanagement;
-
 /**
- * @author jtulip
+ *  This interface has two abstract methods which clear the students details
+ *  and add new student. 
  */
+package datamanagement;
 
 public interface IStudentLister
 {
-    public void clearStudents();
-    public void addStudent(IStudent student);
+    public void clearStudents(); // Clear the student detail
+    
+    /**
+     * This method add the new student and take a Istudent object as a argument.  
+     * @param student student object has a student details.
+     */
+    public void addStudent(IStudent student); 
 }
