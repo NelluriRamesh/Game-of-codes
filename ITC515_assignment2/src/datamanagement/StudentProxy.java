c20f86b2ace1034c8f8ada73805b973ce5ae6d67
BarisPro <bmovoisin@gmail.com>
2016-08-25 22:55:46 +1000

    commit

--- ITC515_assignment2/src/datamanagement/StudentProxy.java
+++ ITC515_assignment2/src/datamanagement/StudentProxy.java
@@ -1,57 +1,74 @@
+Skip to content
+This repository
+Search
+Pull requests
+Issues
+Gist
+ @BarisPro
+ Unwatch 3
+  Star 0
+  Fork 2 NelluriRamesh/Game-of-codes
+ Code  Issues 0  Pull requests 2  Wiki  Pulse  Graphs
+Tree: 2911013d31 Find file Copy pathGame-of-codes/ITC515_assignment2/src/datamanagement/StudentProxy.java
+6e4a168  9 hours ago
+ Nikul More updated
+1 contributor
+RawBlameHistory     59 lines (47 sloc)  1.26 KB
+
 package datamanagement;
 
 public class StudentProxy implements IStudent 
 {
-	private Integer I;
-	private String l;
-    private String Il;
-    private StudentManager lI;
-
-    public StudentProxy( Integer id, String fn, String Il)
-    {
-        this.I = id;
-        this.l = fn;
-        this.Il = Il;
-        this.lI = StudentManager.get();
+	private Integer Id;
+	private String firstName;
+   	private String lastName;
+   	private StudentManager manager;
+   	
+   	public StudentProxy ( Integer id, String fn, String Il )
+   	{
+        this.Id = id;
+        this.firstName = fn;
+       	this.lastName = Il;
+       	this.manager = StudentManager.get();
     }
 
-    public Integer getID()
+   	public Integer getID ()
+   	{ 
+   		return Id; 
+   	}
+    
+   	public String getFirstName () 
     { 
-    	return I; 
+        return firstName;
+    }
+
+   	public String getLastName () 
+    { 
+	   	return lastName; 
+   	}
+
+    public void setFirstName ( String firstName ) 
+    {	
+   		manager.getStudent(Id).setFirstName(firstName);
     }
     
-    public String getFirstName() 
-    { 
-        return l;
-    }
-
-    public String getLastName() 
-    { 
-    	return Il; 
-    }
-
-    public void setFirstName(String firstName) 
-    {
-    	lI.getStudent(I).setFirstName(firstName);
+    public void setLastName ( String lastName ) 
+   	{
+        manager.getStudent(Id).setLastName(lastName);
     }
     
-    public void setLastName(String lastName) 
+    public void addUnitRecord ( IStudentUnitRecord record )
     {
-        lI.getStudent(I).setLastName(lastName);
-    }
+       manager.getStudent(Id).addUnitRecord(record);
+   	}
     
-    public void addUnitRecord(IStudentUnitRecord record)
+    public IStudentUnitRecord getUnitRecord ( String unitCode )
     {
-        lI.getStudent(I).addUnitRecord(record);
-    }
-    
-    public IStudentUnitRecord getUnitRecord(String unitCode)
-    {
-        return lI.getStudent(I).getUnitRecord(unitCode);
+    	return manager.getStudent(Id).getUnitRecord(unitCode);
     }
 
-    public StudentUnitRecordList getUnitRecords() 
+    public StudentUnitRecordList getUnitRecords () 
     { 
-    	return lI.getStudent(I).getUnitRecords();
+    	return manager.getStudent(Id).getUnitRecords();
     }
 }
