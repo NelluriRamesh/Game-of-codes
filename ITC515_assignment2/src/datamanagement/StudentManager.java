d9efd625088e18c9fc9ea0d5e144bd7b60c194ac
Nikul <Nikul@192.168.1.8>
2016-08-25 15:01:27 +1000

    Added more comments in the StudentManager.java and StudentProxy.java files.

--- ITC515_assignment2/src/datamanagement/StudentManager.java
+++ ITC515_assignment2/src/datamanagement/StudentManager.java
@@ -5,42 +5,69 @@ import java.util.List;
 
 public class StudentManager
 {
-    private static StudentManager self = null;    
-    private StudentMap sm;
-    private java.util.HashMap<String, StudentMap> um;
+    private static StudentManager self = null;    		// Initialized the Student manager as a null.
+    private StudentMap sm;								// Declare the StudentMap instance variable. 
+    private java.util.HashMap<String, StudentMap> um;	// Declared the Hash map variable. 
+    
+    // Return the value of self variable, if self is null then it create the StudentManger object. 
     public static StudentManager get()
     {
         if (self == null) 
-        	self = new StudentManager(); 	
+        	self = new StudentManager (); 	
         return self;
     }
     
-    private StudentManager()
+    // Default constructor, initialized the variables. 
+    private StudentManager ()
     {
-        sm = new StudentMap();
-        um = new java.util.HashMap<>();
+        sm = new StudentMap();				// Create the StudentMap object.
+        um = new java.util.HashMap<>();		// Create the HashMap object.
     }
     
+    /**
+     * getStudent() method return the Student object, if it is null then it will
+     * create the student object by calling createStudent method and passed the
+     * id of the student.
+     * 
+     * @param id	id of the student.
+     * @return		student object.
+     */
     public IStudent getStudent(Integer id)
     {
     	IStudent is = sm.get(id);
-    	return is != null ? is : createStudent(id);
+    	return is != null ? is : createStudent(id); 
     }
 
+    /**
+     * Return the list of the student, return null if it is null. 
+     * 
+     * @param id	student id.
+     * @return		list of student.
+     */
     private Element getStudentElement(Integer id)
     {
+    	// loop continues until the last student in XML file.  
         for (Element el : (List<Element>) XMLManager.getXML().getDocument().getRootElement().getChild("studentTable").getChildren("student")) 
-            if (id.toString().equals(el.getAttributeValue("sid"))) 
-            		return el;
+            if (id.toString().equals(el.getAttributeValue("sid")))	// Compare student id with id stored in XML file. 
+            		return el;										
         return null;
-     }
-     
+    }
+    
+    /**
+     * Return the student object, otherwise throw the RuntimeException.
+     * 
+     * @param id	Student id.
+     * @return		Student object.
+     * @throws	RuntimeException	If student is not in file.
+     */
     private IStudent createStudent(Integer id)
     {
         IStudent is;
-        Element el = getStudentElement(id);
+        Element el = getStudentElement(id);		// Get the elements of the student.
         
-        if (el != null)
+       
+        // If element is not null then create the student object and put into the StudentMap.  
+        if (el != null)	
         {
             StudentUnitRecordList rlist = StudentUnitRecordManager.instance().getRecordsByStudent(id);
             is = new Student(new Integer(el.getAttributeValue("sid")),el.getAttributeValue("fname"),el.getAttributeValue("lname"),rlist);
@@ -50,6 +77,13 @@ public class StudentManager
         throw new RuntimeException("DBMD: createStudent : student not in file");
     }
     
+    /**
+     * Return the StudentProxy object.
+     * 
+     * @param id	Student id.
+     * @return		StudentProxy object.
+     * @throws	RuntimeException	If student is not found in XML file.
+     */
     private IStudent createStudentProxy(Integer id) 
     {
         Element el = getStudentElement(id);
@@ -58,7 +92,13 @@ public class StudentManager
         
         throw new RuntimeException("DBMD: createStudent : student not in file");
     }
-
+    
+    /**
+     * Return the StudentMap, if it is null then create it and return.
+     *  
+     * @param uc	Unitcode.
+     * @return		StudentMap.
+     */
     public StudentMap getStudentsByUnit(String uc)
     {
         StudentMap s = um.get(uc);
--- ITC515_assignment2/src/datamanagement/StudentProxy.java
+++ ITC515_assignment2/src/datamanagement/StudentProxy.java
@@ -3,11 +3,19 @@ package datamanagement;
 
 public class StudentProxy implements IStudent 
 {
-	private Integer Id;
-	private String firstName;
-   	private String lastName;
-   	private StudentManager manager;
+	// All data field are private members.
+	private Integer Id;				// Student ID.
+	private String firstName;		// Student's first name.
+   	private String lastName;		// Student's last name.
+   	private StudentManager manager; // Student manager.
    	
+   	/**
+   	 * StudentProxy constructor initialize the the student object.
+   	 * @param id	Student ID.
+   	 * @param fn	Student's first name.
+   	 * @param Il	Student's last name.
+   	 * Current object called the student manager's get() method to get manager object. 
+   	 */
    	public StudentProxy ( Integer id, String fn, String Il )
    	{
         this.Id = id;
@@ -16,41 +24,59 @@ public class StudentProxy implements IStudent
        	this.manager = StudentManager.get();
     }
 
+   	/**
+   	 * @return Id	Return the student ID.
+   	 */
    	public Integer getID ()
    	{ 
    		return Id; 
    	}
     
+   	// Return the student's first name as a string.
    	public String getFirstName () 
     { 
         return firstName;
     }
 
+   	// Return the last name of the student as a string
    	public String getLastName () 
     { 
 	   	return lastName; 
    	}
 
+   	/**
+   	 *  Set the first name of the student.
+   	 *  @param fistName	first name of the student.
+   	 *  Get the student id and called the setFirstName method of the Student class.	
+   	 */
     public void setFirstName ( String firstName ) 
     {	
    		manager.getStudent(Id).setFirstName(firstName);
     }
     
+    /**
+   	 *  Set the last name of the student.
+   	 *  @param lastName	last name of the student.
+   	 *  Get the student id and called the setLastName method of the Student class.	
+   	 */
     public void setLastName ( String lastName ) 
    	{
         manager.getStudent(Id).setLastName(lastName);
     }
     
+    // Add the student unit record by calling Student's class method addUnitRecord.
     public void addUnitRecord ( IStudentUnitRecord record )
     {
        manager.getStudent(Id).addUnitRecord(record);
    	}
     
+    // Return the unit record of the student by calling getUnitRecord of Student class method.
     public IStudentUnitRecord getUnitRecord ( String unitCode )
     {
     	return manager.getStudent(Id).getUnitRecord(unitCode);
     }
-
+    
+    // Get the list of student's unit records.
     public StudentUnitRecordList getUnitRecords () 
     { 
     	return manager.getStudent(Id).getUnitRecords();
